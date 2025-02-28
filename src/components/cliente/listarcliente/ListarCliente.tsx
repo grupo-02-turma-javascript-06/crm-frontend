a2b20a815b901d77da1a2d10e4fa5cc868699ef2
Jessica Rosário <jessica.rosario4@gmail.com>
2025-02-28 01:03:19 -0300

    :sparkle: feat: Cria barra de pesquisa e filtragem

--- src/components/cliente/listarcliente/ListarCliente.tsx
+++ src/components/cliente/listarcliente/ListarCliente.tsx
@@ -1,8 +1,10 @@
 import { useEffect, useState } from "react"
-import Cliente from "../../../models/Cliente"
 import { InfinitySpin } from "react-loader-spinner";
+import { listar } from "../../../services/Service";
+import Cliente from "../../../models/Cliente";
+import { MagnifyingGlass, Plus } from "@phosphor-icons/react";
 
-function ListarClientes() {
+function ListarCliente() {
     const [clientes, setClientes] = useState<Cliente[]>([])
     const [isLoading, setIsLoading] = useState<boolean>(false)
 
@@ -32,8 +34,41 @@ function ListarClientes() {
                     ariaLabel="infinity-spin-loading"
                     />              
             )}
-            <div className="flex justify-center w-full my-4">
-                <div className="container flex flex-col mx-4">
+            <div className="  w-full md:px-8  min-h-screen pt-8 bg-[#F0F3FF]">
+
+                <div className="container flex flex-col md:flex-row justify-between items-center text-center w-full gap-2">
+                    <div>
+                        <h2 className=" text-3xl font-bold text-gray-900">Todos os Clientes</h2>
+                    </div>
+                    <div className="flex items-center text-center justify-center gap-1 flex-row md:gap-3 md:justify-end">
+                        <div className="flex md:w-1/3 gap-2 relative">
+
+                            <input
+                                type="text"
+                                placeholder="Search"
+                                className="pl-10 w-full pr-4 py-2 border-none bg-white rounded-lg shadow-sm focus:outline-none focus:ring-1 focus:ring-gray-300" />
+
+                            <MagnifyingGlass size={30} className="absolute text-neutral-400 top-1 left-1" />
+
+                        </div>
+                        <div>
+                            <select className="hidden md:block p-2 border-none bg-white rounded-lg shadow-sm focus:outline-none focus:ring-1 focus:ring-gray-300">
+                                <option>A - Z</option>
+                                <option>Z - A</option>
+                            </select>
+                        </div>
+
+                        <div className="w-[80%] md:w-auto">
+                            <button className="bg-[#5932EA] w-auto md:w-auto text-white px-4 py-2 rounded-lg flex justify-center text-center items-center gap-2 hover:bg-[#4A24D9] duration-300 shadow-md">
+                                <Plus size={20} />
+                                Cadastrar Cliente
+                            </button>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div className="container flex flex-col justify-center items-center">
                     {(!isLoading && clientes.length === 0) && (
                         <span className="my-8 text-3xl text-center">
                             Nenhum cliente foi encontrado
@@ -48,9 +83,9 @@ function ListarClientes() {
                         ))}
                     </div>
                 </div>
-            </div>
+            </div >
         </>
     )
 }
 
-export default ListarClientes
+export default ListarCliente
